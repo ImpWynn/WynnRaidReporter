923c1c017ba9cb201c698942784200747592e7c0
cathKitten <cat.schoefmann@drei.at>
2024-12-15 03:09:52 +0100

    More debugging logic, bump version

--- src/main/java/at/cath/events/GuildRaidEvent.kt
+++ src/main/java/at/cath/events/GuildRaidEvent.kt
@@ -26,11 +26,15 @@ private val raidKeywords = raidNames.map { it.substringAfterLast(" ", "") }.toLi
 
 
 class GuildRaidMatcher : EventMatcher<GuildRaid> {
+
+    companion object {
+        const val DEBUG = false
+    }
+
     override fun parse(message: Text): GuildRaid? {
         val raidParticipants = mutableListOf<String>()
         var raidName: String? = null
 
-        // todo: maybe switch to just matching by full msg string
         for (sibling in message.siblings) {
             val msgStr = sibling.string
             when (sibling.style.color?.hexCode) {
@@ -38,30 +42,30 @@ class GuildRaidMatcher : EventMatcher<GuildRaid> {
                 "#FFFF55" -> {
                     // check for renamed players
                     val hoverText = sibling.style.hoverEvent?.getValue(HoverEvent.Action.SHOW_TEXT)
+                    if (DEBUG) logger.info("Found hover text: $hoverText")
                     val hoverName = hoverText?.siblings?.last()?.string
                         ?.substringAfterLast(" ", hoverText.string) ?: hoverText?.string
-                    if (hoverName == null) {
-                        logger.error("Found nickname but could not determine hover name")
-                        break
-                    }
-                    hoverText?.let { raidParticipants.add(hoverName) } ?: raidParticipants.add(msgStr)
+                    hoverName?.let { raidParticipants.add(hoverName) } ?: raidParticipants.add(msgStr)
                 }
                 // check for raid keyword match
                 "#00AAAA" -> {
                     raidName = raidKeywords.withIndex().find { msgStr.contains(it.value) }?.let {
                         raidNames.getOrNull(it.index)
                     }
-
+                    // no need to scan the rest of the message if we found the raid name
                     if (raidName != null) break
                 }
             }
         }
 
-        if (raidParticipants.size != 4)
-            return null
+        if (raidName == null) return null
+        if (DEBUG) logger.info("Raid '$raidName' found with participants: $raidParticipants, original message: $message")
+        if (raidParticipants.size != 4) return null
 
-        val raidReporter =
+        val playerUuid =
             MinecraftClient.getInstance().player?.uuidAsString ?: throw IllegalStateException("Player UUID is null")
-        return raidName?.let { GuildRaid(message, it, raidParticipants, raidReporter) }
+        logger.info("Found raid '$raidName' with participants $raidParticipants")
+        return GuildRaid(message, raidName, raidParticipants, playerUuid)
+
     }
 }
